0289408d974e4d6cd659aea729129982d2d8dc22
MadsVestergaard <45486626+MadsVestergaard@users.noreply.github.com>
2020-08-20 21:27:50 +0200

    index table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,7 +21,7 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
-            $table->string('songs');
+            $table->index(['songs'])
 
         });
     }
