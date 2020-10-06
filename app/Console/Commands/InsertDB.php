e3a1e4032924737f427f40d33b55cf1d650bc428
Mads <201805909@post.au.dk>
2020-10-07 01:55:30 +0200

    Whoops v2

--- app/Console/Commands/InsertDB.php
+++ app/Console/Commands/InsertDB.php
@@ -50,18 +50,14 @@ class InsertDB extends Command
         $options = $this->options();
         if (file_exists($options['file'])) 
         {
-            $query = sprintf("
-                LOAD DATA LOCAL INFILE '%s' INTO TABLE songlist
+            $query = sprintf("LOAD DATA LOCAL INFILE '%s' INTO TABLE test", $options['file']) .
+        "FIELD TERMINATED BY ';'
 
-                FIELD TERMINATED BY ';'
+        ENCLOSED BY '\"'
 
-                ENCLOSED BY '\"'
+        LINES TERMINATED BY '\r\n'
 
-                LINES TERMINATED BY '\r\n'
-
-                IGNORE 1 LINES (No, Timestamp(UTC), Album, Duration, Program Title, Score, Label, Release Date, ISRC, ISWC, UPC, Deezer, Spotify, Youtube, Composers, Lyricists, Publishers);",
-                $options['file']
-            );
+        IGNORE 1 LINES (No, Timestamp(UTC), Album, Duration, Program Title, Score, Label, Release Date, ISRC, ISWC, UPC, Deezer, Spotify, Youtube, Composers, Lyricists, Publishers);";
             
             echo $query . "\r\n";
             
