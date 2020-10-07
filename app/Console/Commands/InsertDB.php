ccc29505cc46f3c9f673be8e511ab291b3f7eb13
Mads <201805909@post.au.dk>
2020-10-07 02:06:40 +0200

    removed utc LMAO

--- app/Console/Commands/InsertDB.php
+++ app/Console/Commands/InsertDB.php
@@ -55,7 +55,7 @@ class InsertDB extends Command
         FIELDS TERMINATED BY ';'
         ENCLOSED BY '\"'
         LINES TERMINATED BY '\r\n'
-        IGNORE 1 LINES (No, Timestamp" . '\(' . "UTC" . '\)' . ", Album, Duration, Program Title, Score, Label, Release Date, ISRC, ISWC, UPC, Deezer, Spotify, Youtube, Composers, Lyricists, Publishers);";
+        IGNORE 1 LINES (No, Timestamp, Album, Duration, Program Title, Score, Label, Release Date, ISRC, ISWC, UPC, Deezer, Spotify, Youtube, Composers, Lyricists, Publishers);";
             
             echo $query . "\r\n";
             
