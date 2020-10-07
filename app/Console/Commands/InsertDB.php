1c583925cbdf874dc67626ecf5a810c1586230da
Mads <201805909@post.au.dk>
2020-10-07 03:41:05 +0200

    end it

--- app/Console/Commands/InsertDB.php
+++ app/Console/Commands/InsertDB.php
@@ -51,14 +51,11 @@ class InsertDB extends Command
         $options = $this->options();
         if (file_exists($options['file'])) 
         {
-            $query = sprintf("LOAD DATA LOCAL INFILE '%s'", $options['file']) .
+            $query = sprintf("LOAD DATA LOCAL INFILE '%s' INTO TABLE quantitune.test", $options['file']) .
         "
-        INTO TABLE test
-        FIELDS TERMINATED BY ';'
-        ENCLOSED BY '\"'
-        LINES TERMINATED BY '\r\n'
-        IGNORE 1 LINES
-        (Title, Played_Duration, Artist, ACRID);";
+        FIELDS TERMINATED BY ';' ENCLOSED BY '\"' LINES TERMINATED BY '\r\n' IGNORE 1 LINES
+        (@No, @Timestamp, @TitleCol, @ArtistCol, @Album, @Duration, @Played_DurationCol, @ProgramTitle, @Score, @Label, @ReleaseDate, @ACRIDCol, @ISRC, @ISWC, @UPC, @Deezer, @Spotify, @Youtube, @Composers, @Lyricists, @Publishers)
+        SET Title=@TitleCol, Artist=@ArtistCol, Played_Duration=@Played_DurationCol, ACRID=@ACRIDCol;";
 
         //$query = sprintf("INSERT INTO test (Title, Played_Duration, Artist, ACRID) VALUES ('%s', %d, '%s', '%s')", "hello", 10000, "world", "absoadism");
         
