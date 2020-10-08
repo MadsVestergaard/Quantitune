84dc6522a3169684cf76f721a40e08865eee6101
Mads <201805909@post.au.dk>
2020-10-08 21:52:10 +0200

    asda

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -12,7 +12,7 @@ class TestController extends Controller
         
         //$testsongs = Test::all();
         //$testsongs = Song::orderBy('songName', 'desc')-> get();
-        $testsongs = Test::where('artist', 'Høker')-> get();
+        $testsongs = Test::where('artist', 'Clara')-> get();
         //$testsongs = Song::latest();
         
         return view('test.index', [
