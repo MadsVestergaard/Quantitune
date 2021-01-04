74ee54998af5b127d9e25f0a728517e745598346
Mads <201805909@post.au.dk>
2021-01-04 02:33:07 +0100

    Title fixed

--- resources/views/test/index.blade.php
+++ resources/views/test/index.blade.php
@@ -17,10 +17,7 @@
             @endif
 
             <div class="content">
-                <div class="title m-b-md">
-                    Songs from {{ $test>Artist }}
                 
-                </div>
 
                 @foreach($testsongs as $test)
                 
@@ -30,6 +27,10 @@
                     <div>
                         {{ $test->Artist}}  - {{ $test->Title}} - {{ $test->Played_Duration}}
                     </div>
+                    <div class="title m-b-md">
+                    Songs from {{ $test>Artist }}
+                
+                </div>
 
                 @endforeach
 
