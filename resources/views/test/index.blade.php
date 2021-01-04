5243a87f394d6bdf4d10b77ded411f9850a13a4b
Mads <201805909@post.au.dk>
2021-01-04 02:35:07 +0100

    asd

--- resources/views/test/index.blade.php
+++ resources/views/test/index.blade.php
@@ -17,8 +17,7 @@
             @endif
 
             <div class="content">
-                <div class="title m-b-md">
-                    Songs from {{ $testsongs ->Artist }}
+                
                 
                 </div>
 
@@ -30,6 +29,8 @@
                     <div>
                         {{ $test->Artist}}  - {{ $test->Title}} - {{ $test->Played_Duration}}
                     </div>
+                    <div class="title m-b-md">
+                    Songs from {{ $test ->Artist }}
 
                 @endforeach
 
